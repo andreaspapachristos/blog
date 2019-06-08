5d36a04f5eecd1f82f33837af3278514b00900a8
master <papachristos.andreas@gmail.com>
2019-06-08 12:26:43 +0300

    kainourgia klasi gia post apo arxeio

--- index/views.py
+++ index/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from .models import Post
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from django.contrib.auth import models
 
 # Create your views here.
@@ -22,6 +22,13 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+
+class CreatePost(CreateView):
+    model = Post
+
+    def readFile(self, model):
+        model.instance.author = self.request.user
+        return super().readFile(model)
 
 
 
